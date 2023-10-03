3cbf8fec1bae57777ddec686c2c2c3de68f1a780
ariellejohnson808 <146870658+ariellejohnson808@users.noreply.github.com>
2023-10-03 13:26:26 -0400

    Add files via upload

---
+++ HW5(T+H).py
@@ -0,0 +1,39 @@
+# Tortise and Hare Project
+# 
+# Name(s): Arielle Johnson 
+
+if True:
+    Print('BANG!!!' n/ 'AND THEYRE OFF!!!!!')
+Else:
+pass
+
+# Tortise Code
+
+def Fast_plod():
+    pass
+# 50% + 3 R squares (s.)
+def Slip():
+    pass
+    #30% + 6 L s.
+def Slow_plod():
+    pass
+# 30% + 1 R s.
+
+
+# Hare Code
+
+def Sleep():
+    pass
+# 20% + No move
+def Big_hop():
+    pass
+# 20% + 9 R s. 
+def Big_slip():
+    pass
+# 10% + 12 L s. 
+def Small_hop():
+    pass
+# 30% + 1 R s.
+def Small_slip():
+    pass 
+# 20% + 2 L s. 
